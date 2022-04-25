813d89893ce13b9505a4bb6109234b86ead78137
takashi247 <takashi247@gmail.com>
2022-04-25 09:25:30 +0900

    add stack

---
+++ include/stack.hpp
@@ -0,0 +1,83 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "vector.hpp"
+
+template <class _Tp, class _Container = ft::vector<_Tp> >
+class stack {
+ public:
+  typedef _Container                               container_type;
+  typedef typename container_type::value_type      value_type;
+  typedef typename container_type::size_type       size_type;
+
+ protected:
+  container_type c;
+
+ public:
+  // Default constructor
+  stack() : c() {}
+
+  explicit stack(const container_type& __c) : c(__c) {}
+
+  stack(const stack& __q) : c(__q.c) {}
+
+  stack& operator=(const stack& __q) {
+    if (this != &__q) {
+      c = __q.c;
+    }
+    return *this;
+  }
+
+  virtual ~stack();
+
+  bool empty() const { return c.empty(); }
+
+  size_type size() const { return c.size(); }
+
+  value_type& top() { return c.back(); }
+
+  const value_type& top() const { return c.back(); }
+
+  void push(const value_type& __v) { c.push_back(__v); }
+
+  void pop() { c.pop_back(); }
+
+};
+
+template <class _Tp, class _Container>
+bool
+operator==(const stack<_Tp, _Container>& __x, const stack<_Tp, _Container>& __y) {
+  return __x.c == __y.c;
+}
+
+template <class _Tp, class _Container>
+bool
+operator< (const stack<_Tp, _Container>& __x, const stack<_Tp, _Container>& __y) {
+  return __x.c < __y.c;
+}
+
+template <class _Tp, class _Container>
+bool
+operator!=(const stack<_Tp, _Container>& __x, const stack<_Tp, _Container>& __y) {
+  return !(__x == __y);
+}
+
+template <class _Tp, class _Container>
+bool
+operator> (const stack<_Tp, _Container>& __x, const stack<_Tp, _Container>& __y) {
+  return __y < __x;
+}
+
+template <class _Tp, class _Container>
+bool
+operator>=(const stack<_Tp, _Container>& __x, const stack<_Tp, _Container>& __y) {
+  return !(__x < __y);
+}
+
+template <class _Tp, class _Container>
+bool
+operator<=(const stack<_Tp, _Container>& __x, const stack<_Tp, _Container>& __y) {
+  return !(__y < __x);
+}
+
+#endif // STACK_HPP
